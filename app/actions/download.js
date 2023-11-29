cde7263bfd1ee336f8f65f5a5051de237c3e9994
Omar Melendrez <omar.melendrez@gmail.com>
2023-11-29 16:56:37 -0300

    Abstract process to functions

--- app/actions/download.js
+++ app/actions/download.js
@@ -11,8 +11,11 @@ const localAssets = process.env.LOCAL_ASSETS_URL
 
 const remoteAssets = process.env.REMOTE_ASSETS_URL
 
+// const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
 const downloadDocument = function (url, dest, cb) {
-  const destination = path.join(__dirname, '..', dest)
+  const localAssetsServer = process.env.LOCAL_ASSETS_FOLDER
+  const destination = path.join(localAssetsServer, dest)
   let file = fs.createWriteStream(destination)
   let request = https
     .get(url, function (response) {
@@ -34,6 +37,7 @@ const downloadDocument = function (url, dest, cb) {
 const queryServers = (fileName, f) =>
   new Promise((resolve) =>
     (async () => {
+      // await delay(1000)
       const file = `${f.folderName}/${fileName}`
       const localUrl = `${localAssets}${file}`
       const remoteUrl = `${remoteAssets}${file}`
@@ -49,32 +53,23 @@ const queryServers = (fileName, f) =>
             console.log(err)
           }
         })
-        resolve({ file })
+        resolve(`${f.root}/${file}`)
       } else {
         resolve()
       }
     })()
   )
 
-const down = (folders) =>
+const doTrainingDocuments = (folders) =>
   new Promise((resolve) =>
     (async () => {
-      const learnerDocuments = folders.filter(
-        (l) => l.filenameFrom === FILENAME_FROM.BADGE
-      )
-
-      const trainingDocuments = folders.filter(
-        (t) => t.filenameFrom === FILENAME_FROM.TRAINING_ID
-      )
-
       const [trainings] = await pool.query(
         'SELECT DISTINCT training FROM training_tracking WHERE DATEDIFF(NOW(), updated)<?;',
         DAYS_TO_UPDATE
       )
 
-      const [learners] = await pool.query(
-        'SELECT DISTINCT badge FROM learner WHERE id IN (SELECT learner FROM training WHERE id IN (SELECT DISTINCT training FROM training_tracking WHERE DATEDIFF(NOW(), updated)<?));',
-        DAYS_TO_UPDATE
+      const trainingDocuments = folders.filter(
+        (t) => t.filenameFrom === FILENAME_FROM.TRAINING_ID
       )
 
       for (const t of trainings) {
@@ -87,6 +82,21 @@ const down = (folders) =>
           }
         }
       }
+      resolve('Training documents completed!')
+    })()
+  )
+
+const doLearnerDocuments = (folders) =>
+  new Promise((resolve) =>
+    (async () => {
+      const [learners] = await pool.query(
+        'SELECT DISTINCT badge FROM learner WHERE id IN (SELECT learner FROM training WHERE id IN (SELECT DISTINCT training FROM training_tracking WHERE DATEDIFF(NOW(), updated)<?));',
+        DAYS_TO_UPDATE
+      )
+
+      const learnerDocuments = folders.filter(
+        (l) => l.filenameFrom === FILENAME_FROM.BADGE
+      )
 
       for (const l of learners) {
         for (const f of learnerDocuments) {
@@ -97,8 +107,54 @@ const down = (folders) =>
           }
         }
       }
+      resolve('Learner documents completed!')
+    })()
+  )
 
-      resolve('Process completed')
+const doLearnerPhoto = (folders) =>
+  new Promise((resolve) =>
+    (async () => {
+      const destination = path.join(__dirname, '..', 'uploads', 'pictures')
+
+      const lastPhoto = await fs
+        .readdirSync(destination)
+        .reverse()[0]
+        .split('.')[0]
+
+      const [photos] = await pool.query(
+        'SELECT badge FROM learner WHERE badge>?;',
+        lastPhoto
+      )
+
+      const m = folders.find((f) => f.folderName === 'pictures')
+
+      for (const p of photos) {
+        const fileName = `${p.badge}${m.fileExtension}`
+        const resp = await queryServers(fileName, m)
+        if (resp) {
+          console.log(resp)
+        }
+      }
+      resolve('Leaner photos completed!')
+    })()
+  )
+
+const down = (folders) =>
+  new Promise((resolve) =>
+    (async () => {
+      const t = await doTrainingDocuments(folders)
+      console.log(t)
+      console.log()
+
+      const l = await doLearnerDocuments(folders)
+      console.log(l)
+      console.log()
+
+      const p = await doLearnerPhoto(folders)
+      console.log(p)
+      console.log()
+
+      resolve('Download process completed successfuly.')
     })()
   )
 
